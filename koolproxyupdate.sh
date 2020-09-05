d266b1834168ef5d0251521d19587ad6c3ba4c57
wclmgcd <13980437+wclmgcd@users.noreply.github.com>
2020-09-05 08:14:31 +0800

    规则地址变更，主程序更新

--- koolproxyupdate.sh
+++ koolproxyupdate.sh
@@ -1,13 +1,13 @@
-#wget 'https://kprule.com/kp.dat' -O files/usr/share/koolproxy/data/rules/kp.dat
-#wget 'https://kprule.com/koolproxy.txt' -O files/usr/share/koolproxy/data/rules/koolproxy.txt
-#wget 'https://kprule.com/daily.txt' -O files/usr/share/koolproxy/data/rules/daily.txt
-#wget 'https://kprule.com/user.txt' -O files/usr/share/koolproxy/data/user.txt
-#wget 'https://koolproxy.com/downloads/arm' -O files/bin/arm
-#wget 'https://koolproxy.com/downloads/i386' -O files/bin/i386
-#wget 'https://koolproxy.com/downloads/mips' -O files/bin/mips
-#wget 'https://koolproxy.com/downloads/mipsel' -O files/bin/mipsel
-#wget 'https://koolproxy.com/downloads/x86_64' -O files/bin/x86_64
-#chmod +x files/bin/*
+wget 'https://raw.githubusercontent.com/houzi-/CDN/master/kp.dat' -O files/usr/share/koolproxy/data/rules/kp.dat
+wget 'https://raw.githubusercontent.com/houzi-/CDN/master/koolproxy.txt' -O files/usr/share/koolproxy/data/rules/koolproxy.txt
+wget 'https://raw.githubusercontent.com/houzi-/CDN/master/daily.txt' -O files/usr/share/koolproxy/data/rules/daily.txt
+wget 'https://kprule.com/user.txt' -O files/usr/share/koolproxy/data/user.txt
+wget 'https://raw.githubusercontent.com/houzi-/CDN/master/binary/v3.8.5/arm' -O files/bin/arm
+wget 'https://raw.githubusercontent.com/houzi-/CDN/master/binary/v3.8.5/i386' -O files/bin/i386
+wget 'https://raw.githubusercontent.com/houzi-/CDN/master/binary/v3.8.5/mips' -O files/bin/mips
+wget 'https://raw.githubusercontent.com/houzi-/CDN/master/binary/v3.8.5/mipsel' -O files/bin/mipsel
+wget 'https://raw.githubusercontent.com/houzi-/CDN/master/binary/v3.8.5/x86_64' -O files/bin/x86_64
+chmod +x files/bin/*
 
 wget https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt -O- | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/0\.0\.0\.0:' > files/usr/share/koolproxy/dnsmasq.adblock
 sed -i '/youku/d' files/usr/share/koolproxy/dnsmasq.adblock
